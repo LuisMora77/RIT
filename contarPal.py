f18cd5dca4090c6ca4c5b7d6c115b820313c0c78
LuisMora77 <luismorah77@gmail.com>
2017-10-28 23:22:01 -0600

    casi listo
    Solo falta aplicar para cuantos directorios y agregar información al archivo de salida

--- contarPal.py
+++ contarPal.py
@@ -11,46 +11,39 @@ inicio = time() # iniciar a contar segundos desde que se inician los algoritmos
 lista = os.listdir("C:\\Users\\Luis Mora\\Desktop\\nltk-3.2.5\\spanish_billion_words\\sbw_00")
 os.chdir("C:\\Users\\Luis Mora\\Desktop\\nltk-3.2.5\\spanish_billion_words\\sbw_00")
 #print(lista)
+diccionarioPalabras = {} # diccionario para guardar ocurrencia de palabras
+terminos = {}
+frecuenciaTerminos = {} # diccionario para agrupar palabras según su longitud
 for nombrearchivo in lista: #Recorre cada archivo que se encuentra en el directorio
-    # Abrir archivo para lectura
     with open(nombrearchivo,encoding="utf8") as archivo:
-        diccionarioPalabras = {} # diccionario para guardar ocurrencia de palabras
-        terminos = {}
-        frecuenciaTerminos = {} # diccionario para agrupar palabras según su longitud
+        #a = archivo.read()
+        #archivo2 = sub('[^a-zá-úñ]',"",a)
         for linea in archivo:
-            palabras = linea.split() # separar línea en palabras, por espacio en blanco
+            a = sub(r"(?:^|\s)(?=\S*[^\u0000-\u007FáéíóúüñÁÉÍÓÚÜÑ¿¡])\S+","",linea)
+            palabras = a.split() # separar línea en palabras, por espacio en blanco
             for palabra in palabras:
                 palabra = palabra.lower().strip(".,\n\t") # cambiar a minúscula y quitar puntuación
                 if palabra not in diccionarioPalabras:
                     diccionarioPalabras[palabra] = 1
+                    a = stemmer.stem(palabra)
+                    if a not in terminos:
+                        terminos[a] = [str(palabra)]
+                    else:    
+                        terminos[a] += [str(palabra)]
+                    if a not in frecuenciaTerminos:
+                        frecuenciaTerminos[a] = 1
+                    else:
+                        frecuenciaTerminos[a] +=1
                 else:
                     diccionarioPalabras[palabra] += 1
-            #..............Hasta aqui 9 segundos...
-            for termino in palabras:
-                termino = stemmer.stem(termino)
-            #print(palabra)
-            if termino not in terminos:
-                terminos[termino] = 1
-            else:
-                terminos[termino] += 1
-
-
-                # Agrupar según longitud
-                #longitud = len(palabra)
-                #if longitud not in grupo:
-                 #   grupo[longitud] = { palabra } # Usaremos un conjunto de palabras (sin repetir)
-                #else:
-                 #   grupo[longitud].add( palabra )
-
-    # Cerrar archivo al finalizar
     archivo.close()
-    print("archivo procesado: ", str(nombrearchivo) )
+    #print("archivo procesado: ", str(nombrearchivo), "Tiempo: ", time() - inicio )
 os.chdir("C:\\Users\\Luis Mora\\Desktop\\nltk-3.2.5")
 dos = open("hmm.txt",'w+',encoding = "utf8")
-for llave in diccionarioPalabras:
+for llave in terminos:
     dos.write(str(llave))
     dos.write(str(" : "))
-    dos.write(str(diccionarioPalabras[llave]))
+    dos.write(str(terminos[llave]))
     dos.write('\n')
 dos.close()
 # Mostrar información
